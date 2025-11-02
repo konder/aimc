310ff40a268d6ff6a626bae18975532460e35b90
半臧 <nan.zh@alibaba-inc.com>
2025-11-02 13:23:07 +0800

    add test.py

---
+++ docker/test.py
@@ -0,0 +1,14 @@
+import minedojo
+
+env = minedojo.make(
+    task_id="harvest_wool_with_shears_and_sheep",
+    image_size=(160, 256)
+)
+obs = env.reset()
+for i in range(50):
+    act = env.action_space.no_op()
+    act[0] = 1    # forward/backward
+    if i % 10 == 0:
+        act[2] = 1    # jump
+    obs, reward, done, info = env.step(act)
+env.close()
